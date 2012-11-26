db5b6d3a5c1d74e389c70a3933b69c5d42159682
Bartosz Taudul <bartosz.taudul@game-lion.com>
2012-11-26 15:11:41 +0100

    Read and save duplicates.

--- textureatlas/src/Rect.hpp
+++ textureatlas/src/Rect.hpp
@@ -2,6 +2,7 @@
 #define __RECT_HPP__
 
 #include <string>
+#include <vector>
 
 #include "Types.hpp"
 
@@ -14,14 +15,44 @@ struct Rect
     uint16 x, y, w, h;
 };
 
+struct OffRect
+{
+    uint16 x, y, w, h, ox, oy;
+};
+
 struct BRect
 {
     BRect( uint16 _x, uint16 _y, uint16 _w, uint16 _h, Bitmap* _b, const std::string& _name ) : x( _x ), y( _y ), w( _w ), h( _h ), b( _b ), name( _name ), flip( false ) {}
+    BRect( const BRect& r )
+        : x( r.x )
+        , y( r.y )
+        , w( r.w )
+        , h( r.h )
+        , b( r.b )
+        , name( r.name )
+        , flip( r.flip )
+        , xy( r.xy )
+    {
+    }
+
+    BRect& operator=( const BRect& r )
+    {
+        x = r.x;
+        y = r.y;
+        w = r.w;
+        h = r.h;
+        b = r.b;
+        name = r.name;
+        flip = r.flip;
+        xy = r.xy;
+        return *this;
+    }
 
     uint16 x, y, w, h;
     Bitmap* b;
     std::string name;
     bool flip;
+    std::vector<OffRect> xy;
 };
 
 #endif
