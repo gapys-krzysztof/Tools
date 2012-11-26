3b30566553c833a5d4afcec9c7a00203704366c7
Bartosz Taudul <bartosz.taudul@game-lion.com>
2012-11-26 15:45:39 +0100

    Allow disabling duplicate search.

--- surfsplit/src/Application.cpp
+++ surfsplit/src/Application.cpp
@@ -39,11 +39,13 @@ void Error()
     fprintf( stderr, "Options:\n" );
     fprintf( stderr, " -v     view data layout\n" );
     fprintf( stderr, " -b     set block size (default: 8)\n" );
+    fprintf( stderr, " -n     don't search for duplicates\n" );
     exit( 1 );
 }
 
 bool viewData = false;
 int blockSize = 8;
+bool searchDuplicates = true;
 
 int main( int argc, char** argv )
 {
@@ -65,6 +67,10 @@ int main( int argc, char** argv )
             blockSize = atoi( argv[i+1] );
             i++;
         }
+        else if( CSTR( "-n" ) )
+        {
+            searchDuplicates = false;
+        }
         else
         {
             Error();
@@ -81,100 +87,103 @@ int main( int argc, char** argv )
     std::vector<Rect> r( GenerateGrid( bmp.Size(), blockSize, blockSize ) );
     r = RemoveEmpty( r, &bmp );
 
-    std::vector<int> hist( CalcBroadDuplicates( r, &bmp ) );
-    std::map<int, std::vector<int> > map;
-    for( int i=0; i<hist.size(); i++ )
-    {
-        map[hist[i]].push_back( i );
-    }
-
-    std::vector<Rect> rects;
     std::vector<DupRect> dupes;
-    for( auto it = begin( map ); it != end( map ); ++it )
+    if( searchDuplicates )
     {
-        if( it->second.size() == 1 )
+        std::vector<int> hist( CalcBroadDuplicates( r, &bmp ) );
+        std::map<int, std::vector<int> > map;
+        for( int i=0; i<hist.size(); i++ )
         {
-            rects.push_back( r[*it->second.begin()] );
+            map[hist[i]].push_back( i );
         }
-        else
+
+        std::vector<Rect> rects;
+        for( auto it = begin( map ); it != end( map ); ++it )
         {
-            for( auto lit = begin( it->second ); lit != end( it->second ); ++lit )
+            if( it->second.size() == 1 )
             {
-                auto cit = lit;
-                ++cit;
-                std::vector<Rect> d;
-                while( cit != end( it->second ) )
+                rects.push_back( r[*it->second.begin()] );
+            }
+            else
+            {
+                for( auto lit = begin( it->second ); lit != end( it->second ); ++lit )
                 {
-                    if( AreExactDuplicates( r[*lit], r[*cit], &bmp ) )
+                    auto cit = lit;
+                    ++cit;
+                    std::vector<Rect> d;
+                    while( cit != end( it->second ) )
                     {
-                        d.push_back( r[*cit] );
-                        cit = it->second.erase( cit );
+                        if( AreExactDuplicates( r[*lit], r[*cit], &bmp ) )
+                        {
+                            d.push_back( r[*cit] );
+                            cit = it->second.erase( cit );
+                        }
+                        else
+                        {
+                            ++cit;
+                        }
+                    }
+                    if( d.empty() )
+                    {
+                        rects.push_back( r[*lit] );
                     }
                     else
                     {
-                        ++cit;
+                        Rect& base = r[*lit];
+                        DupRect dr( base );
+
+                        dr.xy.push_back( OffRect( base.x, base.y, base.w, base.h, 0, 0 ) );
+
+                        for( int i=0; i<d.size(); i++ )
+                        {
+                            dr.xy.push_back( OffRect( d[i].x, d[i].y, d[i].w, d[i].h, 0, 0 ) );
+                        }
+
+                        dupes.push_back( dr );
                     }
-                }
-                if( d.empty() )
-                {
-                    rects.push_back( r[*lit] );
-                }
-                else
-                {
-                    Rect& base = r[*lit];
-                    DupRect dr( base );
-
-                    dr.xy.push_back( OffRect( base.x, base.y, base.w, base.h, 0, 0 ) );
-
-                    for( int i=0; i<d.size(); i++ )
-                    {
-                        dr.xy.push_back( OffRect( d[i].x, d[i].y, d[i].w, d[i].h, 0, 0 ) );
-                    }
-
-                    dupes.push_back( dr );
                 }
             }
         }
-    }
 
-    r = rects;
+        r = rects;
 
-    std::vector<DupRect> mdr = Merge( dupes );
-    for( auto it = begin( mdr ); it != end( mdr ); ++it )
-    {
-        it->xy.clear();
-
-        for( auto dit = begin( dupes ); dit != end( dupes ); ++dit )
+        std::vector<DupRect> mdr = Merge( dupes );
+        for( auto it = begin( mdr ); it != end( mdr ); ++it )
         {
-            if( dit->x >= it->x && dit->x + dit->w <= it->x + it->w &&
-                dit->y >= it->y && dit->y + dit->h <= it->y + it->h )
+            it->xy.clear();
+
+            for( auto dit = begin( dupes ); dit != end( dupes ); ++dit )
             {
-                for( auto oit = begin( dit->xy ); oit != end( dit->xy ); ++oit )
+                if( dit->x >= it->x && dit->x + dit->w <= it->x + it->w &&
+                    dit->y >= it->y && dit->y + dit->h <= it->y + it->h )
                 {
-                    it->xy.push_back( OffRect( oit->x, oit->y, oit->w, oit->h, dit->x - it->x, dit->y - it->y ) );
+                    for( auto oit = begin( dit->xy ); oit != end( dit->xy ); ++oit )
+                    {
+                        it->xy.push_back( OffRect( oit->x, oit->y, oit->w, oit->h, dit->x - it->x, dit->y - it->y ) );
+                    }
                 }
             }
         }
-    }
 
-    for( auto it = begin( mdr ); it != end( mdr ); ++it )
-    {
-        std::map<std::pair<int, int>, std::vector<OffRect>> m;
-        for( auto oit = begin( it->xy ); oit != end( it->xy ); ++oit )
+        for( auto it = begin( mdr ); it != end( mdr ); ++it )
         {
-            int bx = ((int)oit->x - (int)it->x)/ (int)it->w;
-            int by = ((int)oit->y - (int)it->y) / (int)it->h;
-            m[std::make_pair( bx, by )].push_back( *oit );
+            std::map<std::pair<int, int>, std::vector<OffRect>> m;
+            for( auto oit = begin( it->xy ); oit != end( it->xy ); ++oit )
+            {
+                int bx = ((int)oit->x - (int)it->x)/ (int)it->w;
+                int by = ((int)oit->y - (int)it->y) / (int)it->h;
+                m[std::make_pair( bx, by )].push_back( *oit );
+            }
+            it->xy.clear();
+            for( auto mit = begin( m ); mit != end( m ); ++mit )
+            {
+                auto mo = Merge( mit->second );
+                std::copy( begin( mo ), end( mo ), std::back_inserter( it->xy ) );
+            }
         }
-        it->xy.clear();
-        for( auto mit = begin( m ); mit != end( m ); ++mit )
-        {
-            auto mo = Merge( mit->second );
-            std::copy( begin( mo ), end( mo ), std::back_inserter( it->xy ) );
-        }
-    }
 
-    dupes = mdr;
+        dupes = mdr;
+    }
 
     r = Merge( r );
     r = CropEmpty( r, &bmp );
