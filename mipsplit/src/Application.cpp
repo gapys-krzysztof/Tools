4aecec2a13707d254c2f2949e456c2bb622ace96
Bartosz Taudul <bartosz.taudul@game-lion.com>
2014-08-13 13:20:42 +0200

    Basic splitting for etc and pvr formats.

--- mipsplit/src/Application.cpp
+++ mipsplit/src/Application.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,6 +30,12 @@ void SplitPVR( FILE* f )
     uint32_t version;
     auto read = fread( &version, 1, 4, f );
     if( read != 4 ) ErrSplit();
+    fseek( f, -4, SEEK_CUR );
+
+    int32_t w, h, mips;
+    int32_t fmt;
+    int32_t bpp;
+    int32_t mw, mh;
 
     if( version == 52 )
     {
@@ -37,14 +45,113 @@ void SplitPVR( FILE* f )
         int format = hdr.Flags & 0xFF;
         if( hdr.HeaderSize != 52 || strncmp( hdr.FourCC, "PVR!", 4 ) != 0 || !( format == 0x0C || format == 0x0D || format == 0x18 || format == 0x19 || format == 0x36 ) )
             ErrSplit();
+        if( hdr.NumSurfs != 1 ) ErrSplit();
+        w = hdr.Width;
+        h = hdr.Height;
+        mips = hdr.MipMapCount;
+        bpp = hdr.BitCount;
+        assert( bpp == 2 || bpp == 4 );
+        switch( format )
+        {
+        case 0x0C:
+            fmt = 0x8C03;   // GL_COMPRESSED_RGBA_PVRTC_2BPPV1_IMG
+            break;
+        case 0x0D:
+            fmt = 0x8C02;   // GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG
+            break;
+        case 0x18:
+            fmt = 0x8C01;   // GL_COMPRESSED_RGB_PVRTC_2BPPV1_IMG
+            break;
+        case 0x19:
+            fmt = 0x8C00;   // GL_COMPRESSED_RGB_PVRTC_4BPPV1_IMG
+            break;
+        case 0x36:
+            fmt = 0x9274;   // GL_COMPRESSED_RGB8_ETC2; 0x8D64 for GL_ETC1_RGB8_OES
+            break;
+        default:
+            assert( false );
+        }
     }
     else if( version == H_PVR3 )
     {
         PvrHeaderV3 hdr;
         read = fread( &hdr, 1, sizeof( PvrHeaderV3 ), f );
         if( read != sizeof( PvrHeaderV3 ) ) ErrSplit();
-        if( !( hdr.PixelFormat[1] == 0 && hdr.PixelFormat[0] == 6 ) || !( hdr.PixelFormat[1] == 0 && hdr.PixelFormat[0] <= 3 ) )
+        if( !( hdr.PixelFormat[1] == 0 && hdr.PixelFormat[0] == 6 ) && !( hdr.PixelFormat[1] == 0 && hdr.PixelFormat[0] <= 3 ) )
             ErrSplit();
+        if( hdr.Depth != 1 || hdr.NumSurfs != 1 || hdr.NumFaces != 1 ) ErrSplit();
+        w = hdr.Width;
+        h = hdr.Height;
+        mips = hdr.MipMapCount - 1;
+        fseek( f, hdr.MetaDataSize, SEEK_CUR );
+        switch( hdr.PixelFormat[0] )
+        {
+        case 0:
+            fmt = 0x8C01;
+            bpp = 2;
+            break;
+        case 1:
+            fmt = 0x8C03;
+            bpp = 2;
+            break;
+        case 2:
+            fmt = 0x8C00;
+            bpp = 4;
+            break;
+        case 3:
+            fmt = 0x8C02;
+            bpp = 4;
+            break;
+        case 6:
+            fmt = 0x9274;
+            bpp = 4;
+            break;
+        default:
+            assert( false );
+        }
+    }
+    else
+    {
+        ErrSplit();
+    }
+
+    switch( fmt )
+    {
+    case 0x8C00:
+    case 0x8C02:
+        mw = mh = 8;
+        break;
+    case 0x8C01:
+    case 0x8C03:
+        mw = 16;
+        mh = 8;
+        break;
+    case 0x9274:
+        mw = mh = 4;
+        break;
+    default:
+        assert( false );
+    }
+
+    FILE* o = fopen( ( out + "/meta" ).c_str(), "wb" );
+    fwrite( &w, 1, 4, o );
+    fwrite( &h, 1, 4, o );
+    fwrite( &mips, 1, 4, o );
+    fwrite( &fmt, 1, 4, o );
+    fclose( o );
+
+    for( int i=0; i<=mips; i++ )
+    {
+        FILE* o = fopen( ( out + "/" + std::to_string( i ) ).c_str(), "wb" );
+        const auto size = ( w * h * bpp ) / 8;
+        char* buf = new char[size];
+        auto ret = fread( buf, 1, size, f );
+        if( ret != size ) ErrSplit();
+        fwrite( buf, 1, size, o );
+        fclose( o );
+
+        w = std::max( mw, w/2 );
+        h = std::max( mh, h/2 );
     }
 }
 
@@ -85,6 +192,10 @@ int main( int argc, char** argv )
     {
         fprintf( stderr, "PVR+ is not currently supported.\n" );
         exit( -1 );
+    }
+    else
+    {
+        fseek( f, 0, SEEK_SET );
     }
 
     if( !CreateDirStruct( out ) )
