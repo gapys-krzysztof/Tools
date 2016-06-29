6f34e29f0d73b9f0148904fdb035cc865a1ad1c3
Bartosz Taudul <bartosz.taudul@game-lion.com>
2016-06-29 16:08:08 +0200

    Directory listing.

---
+++ textureatlas/src/Filesystem.cpp
@@ -0,0 +1,70 @@
+#ifdef _WIN32
+#  include <direct.h>
+#  include <windows.h>
+#else
+#  include <dirent.h>
+#endif
+
+#include "Filesystem.hpp"
+
+std::vector<std::string> ListDirectory( const std::string& path )
+{
+    std::vector<std::string> ret;
+
+#ifdef _WIN32
+    WIN32_FIND_DATA ffd;
+    HANDLE h;
+
+    std::string p = path + "\\*";
+    for( unsigned int i=0; i<p.size(); i++ )
+    {
+        if( p[i] == '/' )
+        {
+            p[i] = '\\';
+        }
+    }
+
+    h = FindFirstFile( ( p ).c_str(), &ffd );
+    if( h == INVALID_HANDLE_VALUE )
+    {
+        return ret;
+    }
+
+    do
+    {
+        std::string s = ffd.cFileName;
+        if( s != "." && s != ".." )
+        {
+            if( ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
+            {
+                ret.emplace_back( std::move( s ) );
+            }
+        }
+    }
+    while( FindNextFile( h, &ffd ) );
+
+    FindClose( h );
+#else
+    DIR* dir = opendir( ( m_path + path ).c_str() );
+    if( dir == nullptr )
+    {
+        return ret;
+    }
+
+    struct dirent* ent;
+    while( ( ent = readdir( dir ) ) != nullptr )
+    {
+        std::string s = ent->d_name;
+        if( s != "." && s != ".." )
+        {
+            if( ent->d_type == DT_DIR )
+            {
+                ret.emplace_back( std::move( s ) );
+            }
+        }
+    }
+    closedir( dir );
+#endif
+
+    return ret;
+}
---
+++ textureatlas/src/Filesystem.hpp
@@ -0,0 +1,9 @@
+#ifndef __FILESYSTEM_HPP__
+#define __FILESYSTEM_HPP__
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> ListDirectory( const std::string& path );
+
+#endif
